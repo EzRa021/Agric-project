d7b0dd1360272746c9c10e98c476136c77afdb7d
EzRa021 <112435033+EzRa021@users.noreply.github.com>
2023-02-04 00:18:13 +0000

    Add files via upload

---
+++ Agric proj/script.js
@@ -0,0 +1,145 @@
+function myfunction() {
+  var navbar = document.getElementById('nav');
+  navbar.classList.toggle('show')
+}
+
+
+
+function myfunction1() {
+  const num1 = document.querySelector("#num1");
+  const num2 = document.querySelector("#num2");
+  const num3 = document.querySelector("#num3");
+  const num4 = document.querySelector("#num4");
+
+  const context1 = document.querySelector("#context1");
+  const context2 = document.querySelector("#context2");
+  const context3 = document.querySelector("#context3");
+  const context4 = document.querySelector("#context4");
+
+  context1.style.display = "flex"
+  context1.style.display = "block"
+  context2.style.display = "none"
+  context3.style.display = "none"
+  context4.style.display = "none"
+
+  num4.style.background = "none"
+  num1.style.background = "white"
+  num1.style.color = "black"
+  num2.style.background = "none"
+  num3.style.background = "none"
+
+  num4.style.color = "white"
+  num3.style.color = "white"
+  num2.style.color = "white"
+  num1.style.color = "lawngreen"
+
+
+
+
+
+}
+
+
+function myfunction2() {
+  const num1 = document.querySelector("#num1");
+  const num2 = document.querySelector("#num2");
+  const num3 = document.querySelector("#num3");
+  const num4 = document.querySelector("#num4");
+
+  const context1 = document.querySelector("#context1");
+  const context2 = document.querySelector("#context2");
+  const context3 = document.querySelector("#context3");
+  const context4 = document.querySelector("#context4");
+  context1.style.display = "none"
+  context2.style.display = "block"
+  context3.style.display = "none"
+  context3.style.display = "none"
+  context2.style.display = "flex"
+
+
+  num4.style.background = "none"
+  num1.style.background = "none"
+  num2.style.background = "white"
+  num3.style.background = "none"
+
+  num4.style.color = "white"
+  num3.style.color = "white"
+  num2.style.color = "lawngreen"
+  num1.style.color = "white"
+
+
+
+
+
+
+}
+
+
+function myfunction3() {
+  const num1 = document.querySelector("#num1");
+  const num2 = document.querySelector("#num2");
+  const num3 = document.querySelector("#num3");
+  const num4 = document.querySelector("#num4");
+
+  const context1 = document.querySelector("#context1");
+  const context2 = document.querySelector("#context2");
+  const context3 = document.querySelector("#context3");
+  const context4 = document.querySelector("#context4");
+  context1.style.display = "none"
+  context2.style.display = "none"
+  context3.style.display = "block"
+  context4.style.display = "none"
+
+
+  num4.style.background = "none"
+  num1.style.background = "none"
+  num2.style.background = "none"
+  num3.style.background = "white"
+  num3.style.color = "black"
+
+
+  context3.style.display = "flex"
+  num4.style.color = "white"
+  num3.style.color = "lawngreen"
+  num2.style.color = "white"
+  num1.style.color = "white"
+
+
+
+
+}
+
+
+function myfunction4() {
+  const num1 = document.querySelector("#num1");
+  const num2 = document.querySelector("#num2");
+  const num3 = document.querySelector("#num3");
+  const num4 = document.querySelector("#num4");
+
+  const context1 = document.querySelector("#context1");
+  const context2 = document.querySelector("#context2");
+  const context3 = document.querySelector("#context3");
+  const context4 = document.querySelector("#context4");
+  context1.style.display = "none"
+  context2.style.display = "none"
+  context3.style.display = "none"
+  context4.style.display = "block"
+  context4.style.display = "flex"
+
+
+
+  num4.style.background = "white"
+  num1.style.background = "none"
+  num2.style.background = "none"
+  num3.style.background = "none"
+
+  num4.style.color = "lawngreen"
+  num3.style.color = "white"
+  num2.style.color = "white"
+  num1.style.color = "white"
+
+
+
+
+
+}
